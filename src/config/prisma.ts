e02115fd20ff84ec9663d2edef76e2bcca01b013
Great-360 <pierrerj05@gmail.com>
2026-04-29 21:50:55 +0200

    chore: upgrade prisma and related dependencies to version 7.8.0, update package.json scripts, and modify prisma schema generator configuration

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension.js";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import fs from "node:fs";
